3426135e871c8fa90de6b08d9a7f6f34e2097664
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-03-30 15:34:50 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,10 +1,7 @@
 p, q = map(int, input().split())
 l = []
-for i in range(1, int(p**1/2)+2):
+for i in range(1, p+1):
     if p%i == 0:
         l.append(i)
-        l.append(p//i)
-l = list(set(l))
-l.sort()
 if len(l) < q: print(0)
 else: print(l[q-1])
