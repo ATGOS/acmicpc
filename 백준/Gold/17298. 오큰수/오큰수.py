30dcdae494508ad52f3730ba07c6053fd9a5b799
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-03-31 15:02:21 +0900

    [Gold IV] Title: 오큰수, Time: 1188 ms, Memory: 155548 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,11 +1,9 @@
+import sys
 n = int(input())
-a = list(map(int, input().split()))
-p = [-1] * n
-l = []
-
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+p = [-1] * n;l = []
 l.append(0)
-for i in range(n-1):
-    while len(l) != 0 and a[l[-1]] < a[i+1]:
-        p[l.pop()] = a[i+1]
-    l.append(i+1)
+for i in range(1, n):
+    while len(l) != 0 and a[l[-1]] < a[i]: p[l.pop()] = a[i]
+    l.append(i)
 print(*p)
