6be7824201a0fdfba9ad6a0e3b493dd9a0d49be5
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-01 10:33:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 500 ms, Memory: 59852 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,7 +1,9 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 l = []
 for i in range(n):
-    l.append(list(map(int, input().split())))
+    l.append(list(map(int, input().rstrip().split())))
 sorted_l = sorted(l, key = lambda x : (x, x))
 for i in sorted_l:
     print(*i)
