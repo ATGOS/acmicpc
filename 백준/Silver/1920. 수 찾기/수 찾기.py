f88c7043ddabbb9517945630126a5d6bf1f7b99b
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-06 01:36:07 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,7 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
-lst1 = list(map(int, input().rstrip().split()))
-lst1.sort()
+lst1 = set(map(int, input().split()))
 m = int(input())
-lst2 = list(map(int, input().rstrip().split()))
-
-start = 0; end = len(lst1) - 1 #이분탐색
-
-for i in lst2:
-    start = 0
-    end = len(lst1) - 1
-    while 1:
-        half = (start + end)//2
-        #print(start, end, half)
-        if lst1[half] > i: # 이분탐색의 왼쪽
-            end = half
-        elif lst1[half] < i: # 이분탐색의 오른쪽
-            if half == start:
-                half+=1
-            start = half
-        if lst1[half] == i:
-            print(1)
-            break
-        if end <= start:
-            print(0)
-            break
+lst2 = list(map(int, input().split()))
+for i in range(m):
+    if lst2[i] in lst1: print(1)
+    else: print(0)
