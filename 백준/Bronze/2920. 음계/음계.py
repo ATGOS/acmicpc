87017799beae8e0d71810f7cff897a33f5973647
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-05 17:13:10 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,8 +1,4 @@
 a = list(map(int, input().split()))
-temp = 0
-if sorted(a) == a:
-    print('ascending')
-elif sorted(a, reverse=True) == a:
-    print('descending')
-else:
-    print('mixed')
+if sorted(a) == a: print('ascending')
+elif sorted(a, reverse=True) == a: print('descending')
+else: print('mixed')
