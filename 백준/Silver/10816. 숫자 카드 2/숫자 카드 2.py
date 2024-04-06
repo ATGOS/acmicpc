5a63f2cef0afad9100bf47936a747b5d985b0d9e
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-06 21:18:28 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 836 ms, Memory: 115672 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,11 +1,9 @@
+from collections import Counter
 n = int(input())
-d = {}
 n_l = list(map(int, input().split()))
 m = int(input())
 m_l = list(map(int, input().split()))
-for i in n_l:
-    if i in d: d[i] += 1
-    else: d[i] = 1
+c = Counter(n_l)
 for i in m_l:
-    if i in d: print(d[i], end = ' ')
+    if i in c: print(c[i], end = ' ')
     else: print(0, end = ' ')
