e1c2e169845e7e2aca25278ad33e5542a4bc9192
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-06 19:51:54 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+    a = input()
+    if a.count('(') != a.count(')'):
+        print('NO')
+        continue
+    left = 0; right = 0 # left = '(' right = ')'
+    for j in a:
+        if j == '(':
+            left += 1
+        else:
+            right += 1
+        if left < right:
+            print('NO')
+            break
+    if left == right:
+        print('YES')
