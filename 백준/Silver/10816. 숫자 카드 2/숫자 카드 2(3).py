7d98e72cc75f435de3e005603ec14f6ba200a5e9
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-06 21:22:36 +0900

    Update and rename 숫자 카드 2(3) to 숫자 카드 2(3).py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2(3).py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2(3).py
@@ -1,9 +1,11 @@
-from collections import Counter
 n = int(input())
+d = {}
 n_l = list(map(int, input().split()))
 m = int(input())
 m_l = list(map(int, input().split()))
-c = Counter(n_l)
+for i in n_l:
+    if i in d: d[i] += 1
+    else: d[i] = 1
 for i in m_l:
-    if i in c: print(c[i], end = ' ')
+    if i in d: print(d[i], end = ' ')
     else: print(0, end = ' ')
