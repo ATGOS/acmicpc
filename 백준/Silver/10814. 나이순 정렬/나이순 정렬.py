469833d34fb6989bb2e455ce236b237b1e4e3456
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-07 00:38:31 +0900

    [Silver V] Title: 나이순 정렬, Time: 308 ms, Memory: 64572 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,9 +3,7 @@ input = sys.stdin.readline
 n = int(input())
 l = []
 for i in range(n):
-    a = input().split()
-    a.insert(1, i)
-    l.append(a)
-l.sort(key=lambda x:(int(x[0]), x[1]))
+    l.append(input().split() + [i])
+l.sort(key=lambda x:(int(x[0]), x[2]))
 for i in l:
-    print(i[0], i[2])
+    print(i[0], i[1])
