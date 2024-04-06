3a2172d00236e5eca9f0b805203911c9bdf5ec0a
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-06 14:58:38 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 54880 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,9 @@
 from collections import deque
 n = int(input())
-dq = deque(i+1 for i in range(n))
-if len(dq) == 1: print(*dq)
-else:
-    while 1:
-        dq.popleft()
-        if len(dq) == 1:
-            break
-        dq.append(dq.popleft())
-    print(*dq)
+dq = deque(list(range(1, n+1)))
+while 1:
+    if len(dq) == 1:
+         break
+    dq.popleft()
+    dq.append(dq.popleft())
+print(*dq)
