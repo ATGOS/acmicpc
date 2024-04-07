ce9b47b9570d757410c98d560a6fd3d919ba96d1
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-07 20:36:59 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+s = []
+for i in range(n):
+    a = input().rstrip()
+    if a[:4] == 'push':
+        s.append(int(a[5:]))
+    elif a == 'pop':
+        if s: print(s.pop(0))
+        else: print(-1)
+    elif a == 'size':
+        print(len(s))
+    elif a == 'empty':
+        if s: print(0)
+        else: print(1)
+    elif a == 'front':
+        if s: print(s[0])
+        else: print(-1)
+    else:
+        if s: print(s[-1])
+        else: print(-1)
