87b1af7760783c4a68bd572d866c22d733d98817
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-09 16:04:08 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+    answer = 0
+    k = int(input())
+    n = int(input())
+    l = [[1]*(k+1)]
+    for i in range(n-1):
+        temp_l = [i+2]
+        for j in range(k):
+            temp_l.append(temp_l[-1] + l[-1][j+1])
+        l.append(temp_l)
+    print(l[-1][-1])
