778a482d9b8fbe7e09c228987a0031506d81abc4
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-12 02:20:09 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,20 +1,15 @@
+import sys
+input = sys.stdin.readline
 while 1:
-    a = input()
+    a = input().rstrip()
     if a == '.': break
     l = []
     for i in a:
-        if i == '(' or i == ')' or i == '[' or i == ']':
-            l += i
+        if i == '(' or i == ')' or i == '[' or i == ']': l += i
     while 1:
         k = ''.join(l)
-        if '()' in k:
-            del l[k.index('()'):k.index('()') + 2]
-        elif '[]' in k:
-            del l[k.index('[]'):k.index('[]') + 2]
+        if '()' in k: del l[k.index('()'):k.index('()') + 2]
+        elif '[]' in k: del l[k.index('[]'):k.index('[]') + 2]
         else:
-            if len(l) == 0:
-                print('yes')
-                break
-            else:
-                print('no')
-                break
+            print('yes' if len(l) == 0 else 'no')
+            break
