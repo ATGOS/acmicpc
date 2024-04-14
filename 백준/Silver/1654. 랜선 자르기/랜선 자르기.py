3befb6f06267df4161571e610bb605871b9cc789
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-14 14:43:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,16 +1,14 @@
+import sys
+input = sys.stdin.readline
 k, n = map(int, input().split())
 l = []
-for _ in range(k):
-    l.append(int(input()))
+for _ in range(k): l.append(int(input()))
 l.sort()
 left = 1; right = l[-1]
 while left <= right:
     sum = 0
     middle = (left + right)//2
-    for i in l:
-        sum += (i//middle)
-    if sum < n:
-        right = middle - 1
-    elif sum >= n:
-        left = middle + 1
+    for i in l: sum += (i//middle)
+    if sum < n: right = middle - 1
+    elif sum >= n: left = middle + 1
 print(left-1)
