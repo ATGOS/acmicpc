c97e1c5a349478b5df129afb280f405cc3a5530a
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-20 13:57:41 +0900

    [Silver V] Title: 줄 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.py
@@ -0,0 +1,6 @@
+n = int(input())
+l = []
+for i in range(n): l.append(input())
+if l == sorted(l): print('INCREASING')
+elif l == sorted(l, reverse=True): print('DECREASING')
+else : print('NEITHER')
