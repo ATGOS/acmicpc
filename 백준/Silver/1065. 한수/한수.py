3722dd257b64238b03a4d0dba6dac35be07b5707
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-04-27 11:48:03 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,14 +1,10 @@
 n = int(input())
-summ = 9
-for i in range(10, n+1):
-    temp = int(str(i)[1]) - int(str(i)[0])
-    for j in range(len(str(i))-1):
-        if int(str(i)[j+1]) - int(str(i)[j]) != temp:
-            break
-    else:
-        summ+=1
-
-if n < 10:
-    print(n)
+if n<10: print(n)
 else:
+    summ = 9
+    for i in range(10, n+1):
+        temp = int(str(i)[1]) - int(str(i)[0])
+        for j in range(len(str(i))-1):
+            if int(str(i)[j+1]) - int(str(i)[j]) != temp: break
+        else: summ+=1
     print(summ)
