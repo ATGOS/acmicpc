53d6a42debe51c90032fc1c75299b2ea4a677af4
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-07-25 18:40:51 +0900

    [Silver I] Title: IOIOI, Time: 48 ms, Memory: 11744 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,23 +1,53 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int N, M, answer{ 0 };
-std::string S, temp_S;
+int N, M, S_len;	// S_len = 본문에서 찾을문자열 길이, M = 본문문자열 길이
+std::string S;
 
-// str.substr(시작위치, 길이) 길이생략시 마지막문자까지
-// 이때 길이 = 2N+1
+std::vector<int> getPi(std::string str) {
+	int j{ 0 };
+	std::vector<int> pi(S_len, 0);
+	for (int i = 1; i < S_len; i++) {
+		while (j > 0 && str[i] != str[j])
+			j = pi[j - 1];
+		if (str[i] == str[j])
+			pi[i] = ++j;
+	}
+	return pi;
+}
+
+int kmp(std::string temp_S) {
+	int answer{ 0 };
+	std::vector<int> pi = getPi(temp_S);
+	int j{ 0 };
+
+	for (int i = 0; i < M; i++) {
+		while (j > 0 && S[i] != temp_S[j])
+			j = pi[j - 1];
+		if (S[i] == temp_S[j]) {
+			if (j == S_len - 1) {
+				answer++;
+				j = pi[j];
+			}
+			else {
+				j++;
+			}
+		}
+	}
+	return answer;
+}
 
 int main() {
 	std::cin >> N >> M >> S;
+	S_len = 2 * N + 1;
 
-	for (int i = 0; i < 2 * N + 1; i++) {
+	std::string temp_S;
+	for (int i = 0; i < S_len; i++) {
 		if (i % 2) temp_S += 'O';
 		else temp_S += 'I';
 	}
 
+	std::cout << kmp(temp_S);
 
-	for (int i = 0; i <= M - (2 * N + 1); i++) {
-		if (S.substr(i, 2 * N + 1) == temp_S) answer++;
-	}
-
-	std::cout << answer;
 }
