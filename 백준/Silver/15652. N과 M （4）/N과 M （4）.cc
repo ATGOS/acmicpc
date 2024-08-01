3b1de86d104e7971f6237a23226877f1e21b1362
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-08-01 21:34:54 +0900

    [Silver III] Title: N과 M (4), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int N, M;
+int arr[8]{ 0 };
+
+void dfs(int start, int count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << "\n";
+		return;
+	}
+
+	for (int i = start; i <= N; i++) {
+		arr[count] = i;
+		dfs(i, count + 1);
+	}
+}
+
+int main() {
+	std::cin >> N >> M;
+	dfs(1, 0);
+}
