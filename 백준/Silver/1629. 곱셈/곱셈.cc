37beb2d1d9b3b0f56a4bf9f9e25f03dafa32fcd9
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-08-30 18:25:57 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 using ull = unsigned long long;
 
@@ -7,17 +6,14 @@ ull C;
 
 ull fpow(ull A, ull B) {
 	if (B == 1) {
-		// std::cout << A << " " << B << " " << A % C << "\n";
 		return A % C;
 	}
 	else if (B % 2 == 1) {
 		ull temp{ fpow(A, (B - 1) / 2) };
-		// std::cout << A << " " << B << " " << ((ull)std::pow(fpow(A, (B - 1) / 2) % C, 2) * (A % C)) % C << "\n";
 		return ((temp * temp) % C * (A % C)) % C;
 	}
 	else if (B % 2 == 0) {
 		ull temp{ fpow(A, B / 2) };
-		// std::cout << A << " " << B << " " << (ull)(std::pow(fpow(A, B / 2) % C, 2)) % C << "\n";
 		return (temp * temp) % C;
 	}
 }
