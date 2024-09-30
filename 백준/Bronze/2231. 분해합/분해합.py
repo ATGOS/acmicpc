6b9ac0ebdb3e89b610ec044fc8ba36bcffeb47f9
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-09-30 18:48:22 +0900

    [Bronze II] Title: 분해합, Time: 524 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,8 @@
-prt = 0
+ans = 0
 t = int(input())
 for a in range(t//2, t+1):
     temp = sum((map(int, str(a)))) + a
     if temp == t:
-        prt = a
+        ans = a
         break
-print(prt)
+print(ans)
