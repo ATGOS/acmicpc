6cdee7ae6cc3a45c1698a4099c7d05cf9f15093b
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-11-15 21:16:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+	int N;
+	std::cin >> N;
+	int ans{ 0 };
+
+	int i;
+	for (i = N; i >= 0; i -= 3) {
+		if (!(i % 5)) {
+			ans += (i / 5);
+			ans += (N - i) / 3;
+			break;
+		}
+	}
+	if (i <= 0 && ans == 0) {
+		ans = -1;
+	}
+
+	std::cout << ans;
+}
