b75f24e4adebc8e3e90e1e4bc5fda2d4258fccbe
ATGOS <56113395+ATGOS@users.noreply.github.com>
2024-11-18 19:13:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	int N, K;
+	std::cin >> N >> K;
+
+	std::vector<std::pair<int, int>> vec;	// 무게w, 가치v
+	int* arr = new int[K + 1] {0};
+
+	for (int i = 0; i < N; ++i) {
+		int w, v;
+		std::cin >> w >> v;
+		vec.push_back({ w, v });
+	}
+
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = K; j >= 0; --j) {
+			if (j >= vec[i].first)
+				arr[j] = std::max(arr[j], arr[j - vec[i].first] + vec[i].second);
+		}
+	}
+
+	std::cout << arr[K];
+}
